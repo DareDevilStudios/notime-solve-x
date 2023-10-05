f823d0e7dd20e0017b65f1e2e06b63368a18ee23
DareDevilStudios <studios.daredevil@gmail.com>
2023-10-05 21:37:43 +0530

    button some changes made

--- src/common/UI/SolveQuestion/individual_questions/individual.jsx
+++ src/common/UI/SolveQuestion/individual_questions/individual.jsx
@@ -1,38 +1,59 @@
-import Chat_box from '@/common/components/Chat_box'
-import Common_Button from '@/common/components/Common_Button'
-import Image from 'next/image'
-import React from 'react'
+import Chat_box from "@/common/components/Chat_box";
+import Common_Button from "@/common/components/Common_Button";
+import Image from "next/image";
+import React from "react";
 
 const Individual = () => {
   return (
     <>
-      <div className='bg-black flex flex-row'>
-        <div className='flex flex-col w-1/2 p-8'>
-          <div className='mb-9'>
-            <h1 className='text-4xl font-black text-[#1BEFA0] mb-4'>Query 1</h1>
-            <h1>Delete the node at a given position in a linked list and return a reference to the head node. The head is at position 0. The list may be empty after you delete the node. In that case, return a null value.</h1>
+      <div className="bg-black flex flex-row">
+        <div className="flex flex-col w-1/2 p-8 justify-between">
+          <div className="flex flex-col">
+            <div className="mb-9">
+              <h1 className="text-4xl font-black text-[#1BEFA0] mb-4">
+                Query 1
+              </h1>
+              <h1>
+                Delete the node at a given position in a linked list and return
+                a reference to the head node. The head is at position 0. The
+                list may be empty after you delete the node. In that case,
+                return a null value.
+              </h1>
+            </div>
+            <div className="flex gap-4 items-center">
+              <h1>Posted by @haroldas</h1>
+              <Image
+                src={"/website_screenshots/person.png"}
+                width={35}
+                height={35}
+              />
+            </div>
           </div>
-          <div className='flex gap-4 items-center'>
-            <h1>Posted by @haroldas</h1>
-            <Image src={"/website_screenshots/person.png"} width={35} height={35} />
-          </div>
-          <div>
-            <Common_Button children='TECH STACK/LANGUAGE: C++ 11' colour="#262B30" />
-          </div>
-          <div className='flex flex-row justify-between'>
-            <Common_Button children="25 coins" colour="#EEAA2B" />
-            <Common_Button children=" due on oct 25 " colour="#262B30" />
-          </div>
-          <div>
-            <Common_Button children="sent request to solve" colour="#1BEFA0" />
+          <div className="flex flex-col gap-4 mt-8">
+            <div className="">
+              <Common_Button
+                children="TECH STACK/LANGUAGE: C++ 11"
+                colour="bg-[#262B30]"
+              />
+            </div>
+            <div className="flex flex-row justify-between gap-4">
+              <Common_Button children="25 coins" colour="bg-[#EEAA2B]" />
+              <Common_Button children=" due on oct 25 " colour="bg-[#262B30]" />
+            </div>
+            <div>
+              <Common_Button
+                children="sent request to solve"
+                colour="bg-[#1BEFA0]"
+              />
+            </div>
           </div>
         </div>
-        <div className='w-1/2'>
+        <div className="w-1/2">
           <Chat_box />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Individual
+export default Individual;
--- src/common/components/Chat_box.jsx
+++ src/common/components/Chat_box.jsx
@@ -4,51 +4,69 @@ import { BsFillSendFill } from "react-icons/bs";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { BsThreeDots } from "react-icons/bs";
 import Common_Button from "./Common_Button";
+import DotHeading from "./DotHeading";
 
 const Chat_box = () => {
   return (
     <>
       <div className="bg-[#0D1418] w-full gap-4 ">
-        <div className="h-[60px] bg-black flex justify-between">
-          <div className="flex">
-          <Image
-            className=" ml-5 h-[2.5rem] mt-3"
-            src="/website_screenshots/person.png"
-            alt="photo"
-            width={35}
-            height={10}
-          />
-          
-          
-          <p className="text-white text-xl  ml-3  mt-2">Harold Das</p>
-          <p className="text-white text-xs mt-[2rem] mx-[-6rem]  ">
-            @harold_das
-          </p>
+        <div className="">
+          <DotHeading />
+        </div>
+        <div className="h-[60px] bg-black flex justify-between px-5">
+          <div className="flex items-center gap-4">
+            <Image
+              className="h-[2.5rem]"
+              src="/website_screenshots/person.png"
+              alt="photo"
+              width={35}
+              height={10}
+            />
+
+            <div className="flex flex-col">
+              <p className="text-white text-xl">Harold Das</p>
+              <p className="text-white text-xs">@harold_das</p>
+            </div>
           </div>
-          <div>
-          <BsThreeDotsVertical className=" w-[3rem] fill-white " />
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+              />
+            </svg>
           </div>
         </div>
-        <div className="w-[5rem] h-[1.5rem] bg-gray-800 mt-[12rem]  rounded-xl text-white mx-auto my-auto pt-0.5 text-center text-xs ">
-          Today
-        </div>
-        <div className="flex justify-end">
-          <Common_Button children="found a solution" colour="#1BEFA0" />
-        </div>
-        <div className="w-[5rem] h-[2.5rem] mb-[1.5rem] bg-gray-800 rounded ml-[1rem] mt-[2.5rem]">
-          <BsThreeDots className="fill-white w-[3rem] my-auto mx-auto h-[2.5rem]" />
-        </div>
-        <div className="flex items-center px-3 mt-[2.5] py-2 rounded-lg ">
-          <input
-            type="text"
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-800  rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-          />
-          <button
-            type="submit"
-            className="inline-flex justify-center  p-2  text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 absolute right-2"
-          />
-          <div className="w-[30px] h-[28px] bg-[#1BEFA0] rounded ml-[-2.6rem]">
-            <BsFillSendFill className="text-white ml-2 mt-2" />
+        <div className="h-full">
+          <div className="w-[5rem] h-[1.5rem] bg-gray-800 rounded-xl text-white mx-auto my-auto pt-0.5 text-center text-xs ">
+            Today
+          </div>
+          <div className="flex justify-end w-1/2">
+            <Common_Button children="found a solution" colour="bg-[#1BEFA0]" />
+          </div>
+          <div className="w-[5rem] h-[2.5rem] mb-[1.5rem] bg-gray-800 rounded ml-[1rem] mt-[2.5rem]">
+            <BsThreeDots className="fill-white w-[3rem] my-auto mx-auto h-[2.5rem]" />
+          </div>
+          <div className="flex items-center px-3 mt-[2.5] py-2 rounded-lg ">
+            <input
+              type="text"
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-800  rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            />
+            <button
+              type="submit"
+              className="inline-flex justify-center  p-2  text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 absolute right-2"
+            />
+            <div className="w-[30px] h-[28px] bg-[#1BEFA0] rounded ml-[-2.6rem]">
+              <BsFillSendFill className="text-white ml-2 mt-2" />
+            </div>
           </div>
         </div>
       </div>
