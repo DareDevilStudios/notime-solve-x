81b79f89cfa9990469908782d50ae8dc44fc1018
DareDevilStudios <studios.daredevil@gmail.com>
2023-10-05 21:15:14 +0530

    some changes made

--- src/common/UI/SolveQuestion/individual_questions/individual.jsx
+++ src/common/UI/SolveQuestion/individual_questions/individual.jsx
@@ -1,17 +1,21 @@
 import Chat_box from '@/common/components/Chat_box'
 import Common_Button from '@/common/components/Common_Button'
+import Image from 'next/image'
 import React from 'react'
 
 const Individual = () => {
   return (
     <>
       <div className='bg-black flex flex-row'>
-        <div className='flex flex-col w-1/2 px-8'>
-          <div>
-            <h1 className='text-4xl font-black text-[#1BEFA0]'>Query 1</h1>
+        <div className='flex flex-col w-1/2 p-8'>
+          <div className='mb-9'>
+            <h1 className='text-4xl font-black text-[#1BEFA0] mb-4'>Query 1</h1>
             <h1>Delete the node at a given position in a linked list and return a reference to the head node. The head is at position 0. The list may be empty after you delete the node. In that case, return a null value.</h1>
           </div>
-          <div>Posted by @haroldas</div>
+          <div className='flex gap-4 items-center'>
+            <h1>Posted by @haroldas</h1>
+            <Image src={"/website_screenshots/person.png"} width={35} height={35} />
+          </div>
           <div>
             <Common_Button children='TECH STACK/LANGUAGE: C++ 11' colour="#262B30" />
           </div>
