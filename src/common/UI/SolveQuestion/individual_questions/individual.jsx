e7d817b21452810484f3710418bebd428e3219f1
Annasusan Manjaly B <annasusanmanjaly@gmail.com>
2023-10-05 18:37:54 +0530

    inidividual page

--- src/common/UI/SolveQuestion/individual_questions/individual.jsx
+++ src/common/UI/SolveQuestion/individual_questions/individual.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const individual = () => {
+const Individual = () => {
   return (
-    <div>individual solving request and chat</div>
+    <>
+      <div>
+        <h1 className='text-4xl font'>Query 1</h1>
+        <h1>Delete the node at a given position in a linked list and return a reference to the head node. The head is at position 0. The list may be empty after you delete the node. In that case, return a null value.</h1>
+      </div>
+    </>
   )
 }
 
-export default individual
+export default Individual
